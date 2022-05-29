6e7f81fea41e05ae834cf91cff996c8360b6558c
marcelodscrmv <marcelo.paiva@crmvmg.gov.br>
2022-05-29 13:29:25 -0300

    Teste de tabelas e query R

---
+++ r_docker/testequery.R
@@ -0,0 +1,30 @@
+library(DBI)
+library(tidyverse)
+
+con <- dbConnect(RPostgres::Postgres(), dbname = 'teste',
+                 host = 'kb-sefis-postgres',
+                 port = 5432,
+                 user = 'teste',
+                 password = 'teste123')
+
+dbListTables(con)
+dbListFields(con, "clientes")
+dbReadTable(con, "clientes")
+
+dbReadTable(con, "clientes") %>% dplyr::filter(estado=="TT")
+cliente <- str_c('teste', seq(11, 20))
+estado <- 'FF'
+sexo <- 'M'
+status <- 'Boçãl'
+for (i in 1:10) {
+  query <- str_c("INSERT INTO clientes(cliente, estado, sexo, status) VALUES(",
+                 "'", cliente[i], "', ",
+                 "'", estado, "', ",
+                 "'", sexo, "', ",
+                 "'", status, "'",
+                 ")")
+  sendquery <- dbSendQuery(con, query)
+  dbClearResult(sendquery)
+}
+
+dbReadTable(con, "clientes") %>% dplyr::filter(estado=="FF")
